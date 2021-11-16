1da0db2b1bcd268859a15cf2c7d64e5a55db6463
Jorge García <31393313+SirSykon@users.noreply.github.com>
2021-11-16 18:21:10 +0100

    Delete src directory

--- src/data_initialization.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "This script will ask for some paths in order to prepare the data to be used to fine-tune a network.";
-
-python fine_tuning_utils/partition_dataset.py -x -i ../input/images/ -r 0.1
-echo "Generating tf train record."
-python fine_tuning_utils/generate_tf_record.py -x ../input/images/train -l ../input/annotations/label_map.pbtxt -o ../input/annotations/train.record -i ../input/images/train -c ../input/annotations/train.csv
-echo "Generating tf train record."
-python fine_tuning_utils/generate_tf_record.py -x ../input/images/test -l ../input/annotations/label_map.pbtxt -o ../input/annotations/test.record -i ../input/images/test -c ../input/annotations/test.csv
-
--- src/evaluate.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from coco_evaluation_utils import evaluate
-
-parser = argparse.ArgumentParser(description='Script to execute the coco evaluation for object detection')
-parser.add_argument('-gt', '--ground_truth', help="Path to groundtruth .json annotations file.")
-parser.add_argument('-r', '--results', help="Path to results .json annotations file.")
-args = parser.parse_args()
-
-evaluate(args.ground_truth, args.results)
